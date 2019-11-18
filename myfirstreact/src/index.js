ed7934cd2fd00c087451719d7274884a6f44b788
peter14067 <peter14067@gmail.com>
2019-11-18 23:58:53 +0800

    myfirstreact

--- myfirstreact/src/index.js
+++ myfirstreact/src/index.js
@@ -21,14 +21,14 @@ class Main extends React.Component {
                 
                   <div class="top">
              
-                  
+                  <table >
                       <td><Link to="/" style={{textDecoration:"none",color:"black",marginRight:"10vw"}}>首頁</Link></td>
                       <td><Link to="/about" style={{textDecoration:"none",color:"black",marginRight:"10vw"}}>關於我們</Link></td>
                       <td><Link to="/about" style={{textDecoration:"none",color:"black",marginRight:"10vw"}}>地圖</Link></td>
                       <td><Link to="/Contact" style={{textDecoration:"none",color:"black",marginRight:"10vw"}}>聯絡我們</Link></td>
                       <td><Link to="/about" style={{textDecoration:"none",color:"black",marginRight:"10vw"}}>js作品集</Link></td>
                     
-                  
+                  </table>
                 
                   </div>
                  
