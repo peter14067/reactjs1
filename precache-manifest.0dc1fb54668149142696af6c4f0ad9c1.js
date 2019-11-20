1d1930e4aa4f252b2c6df26d5ed874317825e4a2
peter14067 <peter14067@gmail.com>
2019-11-20 23:47:38 +0800

    Updates

---
+++ precache-manifest.0dc1fb54668149142696af6c4f0ad9c1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3421724093b67d526e944e4d9d9087c",
+    "url": "/reactjs1/index.html"
+  },
+  {
+    "revision": "523aed0a7d6f6bfb220b",
+    "url": "/reactjs1/static/css/main.c7aad762.chunk.css"
+  },
+  {
+    "revision": "3de32d47f65310dfc289",
+    "url": "/reactjs1/static/js/2.b55a03c3.chunk.js"
+  },
+  {
+    "revision": "523aed0a7d6f6bfb220b",
+    "url": "/reactjs1/static/js/main.fdb8a66b.chunk.js"
+  },
+  {
+    "revision": "e8757b5806da09748d2f",
+    "url": "/reactjs1/static/js/runtime-main.b6229466.js"
+  },
+  {
+    "revision": "e083a59ad8978da7aaef0d36458bfff4",
+    "url": "/reactjs1/static/media/pc1.e083a59a.jpg"
+  },
+  {
+    "revision": "dd861778d5e8672b2d246ca543399608",
+    "url": "/reactjs1/static/media/pc11.dd861778.jpg"
+  },
+  {
+    "revision": "9bf338e55ded4f2e3677951c033b255d",
+    "url": "/reactjs1/static/media/pc2.9bf338e5.png"
+  },
+  {
+    "revision": "1f5d80be7a87827088b9f6c5f85f21cd",
+    "url": "/reactjs1/static/media/pc3.1f5d80be.jpg"
+  }
+]);
