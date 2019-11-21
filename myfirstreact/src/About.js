cdcf6104f28c589630c6317d928026f147c93816
peter14067 <peter14067@gmail.com>
2019-11-21 22:57:56 +0800

    myfirstreact

--- myfirstreact/src/About.js
+++ myfirstreact/src/About.js
@@ -1,8 +1,8 @@
 import React from "react"
 import  './about.css'; 
-import Logo from './pc1.jpg';
-import Logo1 from './pc2.png';
-import Logo2 from './pc3.jpg';
+import Logo1 from './112101.jpg';
+import Logo2 from './112102.jpg';
+import Logo3 from './112103.jpg';
 
 class About extends React.Component{
     
@@ -47,10 +47,16 @@ class About extends React.Component{
             
             <div style={{backgroundColor:"#52453C",height:"100vh"}}>
                 <div className="abouttest">
-                家庭成員
+                家庭成員介紹
                 </div>
+                <img class="logo1" src={Logo1} alt="Logo" /> 
+                <img class="logo2" src={Logo2} alt="Logo" /> 
+                <img class="logo3" src={Logo3} alt="Logo" /> 
 
             </div>
+           
+           
+           
             <div className="demo2-bg" >
                          <div className="demo2">About US</div>
             </div>
